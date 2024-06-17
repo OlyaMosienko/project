d7fc89f5deaf0a233c11b6430d83bc747d090ded
OlyaMosienko <olya.ololoeva81@mail.ru>
2024-06-17 14:08:36 +0300

    Rewrite App.js from JSX to JS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,39 @@
-// декларативный стиль - описываем, что хотим получить, но не конкретные действия
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// декларативный стиль - объявление функции, которая не описывает конкретные шаги,
-// а лишь возвращает шаблон HTML
 export const App = () => {
-	return (
-		// декларативный стиль - описываем, что хотим получить на выходе
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{/* код внутри параграфа - императивный стиль,
-				мы описали шаги, как вычислить год,
-				но не написали конкретный */}
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
+	return React.createElement(
+		'div',
+		{
+			className: App,
+		},
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('p', null, new Date().getFullYear()),
+		),
 	);
 };
